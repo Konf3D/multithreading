2efa97d7edda14bab6eea08dadceceafe63338b6
KonfeD <arturmakushev@gmail.com>
2023-06-10 18:42:47 +0700

    Task reworked

--- multithreading/main.cpp
+++ multithreading/main.cpp
@@ -1,47 +1,130 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <thread>
-#include <numeric>
-#include <chrono>
-template<typename T>
-T calculateSum(const std::vector<T>& v, const size_t m) {
-    const size_t n = v.size();
-    std::vector<std::thread> threads(m);
-    std::vector<T> sums(m);
-    const size_t chunkSize = n / m;
-    for (size_t i = 0; i < m; i++) {
-        threads[i] = std::thread([=, &sums]() {
-            const size_t start = i * chunkSize;
-            const size_t end = (i == m - 1) ? n : (i + 1) * chunkSize;
-            sums[i] = std::accumulate(v.begin() + start, v.begin() + end, static_cast<T>(0));
-            });
+#include <future> 
+#include <memory>
+#include <mutex>
+#include<queue>
+// Функция для выполнения быстрой сортировки
+void quickSort(std::vector<int>& array) {
+    if (array.size() <= 1)
+        return;
+
+    int pivot = array[array.size() - 1];
+    std::vector<int> left, right;
+
+    for (size_t i = 0; i < array.size() - 1; i++) {
+        if (array[i] < pivot)
+            left.push_back(array[i]);
+        else
+            right.push_back(array[i]);
     }
-    for (size_t i = 0; i < m; i++) {
-        threads[i].join();
-    }
-    return std::accumulate(sums.begin(), sums.end(), static_cast<T>(0));
+
+    quickSort(left);
+    quickSort(right);
+
+    std::copy(left.begin(), left.end(), array.begin());
+    array[left.size()] = pivot;
+    std::copy(right.begin(), right.end(), array.begin() + left.size() + 1);
 }
-int main()
-{
-    // При использовании бОльшего количества потоков скорость выполнения задачи падает. Мб компилятор добавляет векторизацию.
-    // Тесты выполнены на 6-ядерном процессоре.
-    for (int n = 100; n <= 100000001; n = n * 100)
-    {
-        std::cout << "N = " << n << std::endl;
-        std::vector<int> v(n);
-        for (int i = 0; i < n; ++i) {
-            v[i] = rand() % 10000 - 5000;
+
+// Класс задачи сортировки
+class SortTask {
+public:
+    SortTask(std::vector<int>& data, std::shared_ptr<std::promise<void>> promise)
+        : data_(data), promise_(std::move(promise)) {}
+
+    void operator()() {
+        quickSort(data_);
+        promise_->set_value();
+    }
+
+private:
+    std::vector<int>& data_;
+    std::shared_ptr<std::promise<void>> promise_;
+};
+
+// Пул потоков
+class ThreadPool {
+public:
+    explicit ThreadPool(size_t numThreads) : threads_(numThreads), stop_(false) {
+        for (size_t i = 0; i < numThreads; ++i) {
+            threads_[i] = std::thread(&ThreadPool::threadLoop, this);
         }
-        for (int m = 16; m > 0; m /= 2)
+    }
+
+    ~ThreadPool() {
         {
-            const auto start = std::chrono::high_resolution_clock::now();
-            const int sum = calculateSum<int>(v, m);
-            const auto end = std::chrono::high_resolution_clock::now();
-            const std::string time = " miliseconds";
-            const auto duration = std::chrono::duration_cast<std::chrono::milliseconds > (end - start);
-            std::cout << "Threads: " << m << std::endl << "Duration: " << duration.count() << time << std::endl << "Sum: " << sum << std::endl << std::endl;
+            std::unique_lock<std::mutex> lock(mutex_);
+            stop_ = true;
+            condition_.notify_all();
         }
 
+        for (std::thread& thread : threads_) {
+            thread.join();
+        }
     }
+
+    template <typename Func>
+    std::future<void> submitTask(Func func) {
+        std::shared_ptr<std::promise<void>> promise = std::make_shared<std::promise<void>>();
+        std::future<void> future = promise->get_future();
+
+        {
+            std::unique_lock<std::mutex> lock(mutex_);
+            tasks_.emplace([func, promise]() mutable {
+                func();
+                promise->set_value();
+                });
+        }
+
+        condition_.notify_one();
+        return future;
+    }
+
+private:
+    void threadLoop() {
+        while (true) {
+            std::function<void()> task;
+
+            {
+                std::unique_lock<std::mutex> lock(mutex_);
+                condition_.wait(lock, [this]() { return stop_ || !tasks_.empty(); });
+
+                if (stop_ && tasks_.empty()) {
+                    return;
+                }
+
+                task = std::move(tasks_.front());
+                tasks_.pop();
+            }
+
+            task();
+        }
+    }
+
+    std::vector<std::thread> threads_;
+    std::queue<std::function<void()>> tasks_;
+    std::mutex mutex_;
+    std::condition_variable condition_;
+    bool stop_;
+};
+
+int main() {
+    std::vector<int> data = { 9, 5, 3, 7, 2, 8, 1, 6, 4 };
+
+    ThreadPool pool(std::thread::hardware_concurrency());
+
+    std::future<void> future = pool.submitTask(SortTask(data, std::make_shared<std::promise<void>>()));
+
+    future.wait();  // Ждем завершения сортировки
+
+    std::cout << "Sorted array: ";
+    for (int num : data) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
